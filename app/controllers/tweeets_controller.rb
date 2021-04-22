cc03994c8c79e6da77f20c5ec9fdd3d57189d65f
chasscepts <francischassobetta@gmail.com>
2021-04-22 08:47:35 +0100

    Add User model and implement authorizations

--- app/controllers/tweeets_controller.rb
+++ app/controllers/tweeets_controller.rb
@@ -1,12 +1,13 @@
 class TweeetsController < ApplicationController
   before_action :set_tweeet, only: %i[show edit update destroy]
+  before_action :authenticate_user!, except: [:index, :show]
 
   FollowSuggest = Struct.new(:firstname, :lastname)
 
   # GET /tweeets or /tweeets.json
   def index
     @tweeets = Tweeet.all.order('created_at DESC')
-    @tweeet = Tweeet.new
+    @tweeet = current_user ? current_user.tweeets.build : Tweeet.new
   end
 
   # GET /tweeets/1 or /tweeets/1.json
@@ -14,7 +15,7 @@ class TweeetsController < ApplicationController
 
   # GET /tweeets/new
   def new
-    @tweeet = Tweeet.new
+    @tweeet = current_user.tweeets.build
   end
 
   # GET /tweeets/1/edit
@@ -22,11 +23,11 @@ class TweeetsController < ApplicationController
 
   # POST /tweeets or /tweeets.json
   def create
-    @tweeet = Tweeet.new(tweeet_params)
+    @tweeet = current_user.tweeets.build(tweeet_params)
 
     respond_to do |format|
       if @tweeet.save
-        format.html { redirect_to @tweeet, notice: 'Tweeet was successfully created.' }
+        format.html { redirect_to root_path, notice: 'Tweeet was successfully created.' }
         format.json { render :show, status: :created, location: @tweeet }
       else
         format.html { render :new, status: :unprocessable_entity }
--- app/helpers/tweeets_helper.rb
+++ app/helpers/tweeets_helper.rb
@@ -1,2 +1,9 @@
 module TweeetsHelper
+  def tweeet_avatar(tweeet)
+    gravatar_image_tag(tweeet.user.email, width: 64, height: 64, alt: tweeet.user.username)
+  end
+
+  def user_avatar(user)
+    gravatar_image_tag(user.email, width: 64, height: 64, alt: user.username)
+  end
 end
